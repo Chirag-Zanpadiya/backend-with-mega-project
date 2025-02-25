4d35896ca7a06e76fc2dd5f6c9a344771aafd0f4
Chirag-Zanpadiya <zanpadiyachirag123@gmail.com>
2025-02-25 17:52:25 +0530

    connection of MongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,47 @@
+
+//  ye bhi syntax sahi hai lekin requier import se sath jam nahi raha hai  // require('dotenv').config({path :'./env'});
+
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+
+import dotenv from "dotenv"
+// iffi fns
+// DB se jab bhi bat karo tab try-catch lagao
+
+dotenv.config({
+    path : './env'
+})
+
+
+
+// connectDB wala fns execute kara diya hai
+connectDB();
+
+
+
+
+
+
+
+
+// TODO: first approach
+
+// const app = express();
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//     app.on("error", (error) => {
+//       console.log("ERROR : ", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+
+//   } catch (error) {
+//     console.error("ERROR: ", error);
+//     throw err;
+//   }
+// })();
